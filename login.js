22aa77481cac964e4e4f3163888b9c78b9591871
Dandy Raka <dandyraka13@gmail.com>
2024-03-06 16:30:28 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
+const secretKey = 'JKT48Live';
 let url = window.location.pathname;
-let data = url.split("/")[2];
+//let data = url.split("/")[2];
+let data = url.replace("/wrappedExtWeb/wrappedExtWeb", "");
+//let decode = decodeURIComponent(data);
+const decryptedData = CryptoJS.AES.decrypt(data, secretKey).toString(CryptoJS.enc.Utf8);
 let decode = decodeURIComponent(data);
 
 function toggleTopUpVisibility() {
