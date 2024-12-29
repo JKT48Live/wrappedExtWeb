5a70f28420940d08c5bbe4571f65a811b542c08b
Dandy Raka <dandyraka13@gmail.com>
2024-12-29 19:54:46 +0700

    Change color & Add click to censor topup

--- login.js
+++ login.js
@@ -59,10 +59,16 @@ function createCard(data) {
             ${(data.data.events.lastEvents) ? `
             <b>Last Events:</b><br>${Array.isArray(data.data.events.lastEvents) ? data.data.events.lastEvents.map(event => `- ${event}`).join('<br>') : data.data.events}<br><br>
             ` : data.data.events+"<br><br>"}
-            <b>Total Top-Up:</b><br>${data.data.topUp}<br><br>
+            <b>Total Top-Up:</b><br><span class="total-topup">${data.data.topUp}</span><br><br>
             <center><small><b>#JKT48Wrapped made with ❤️ by JKT48 Live</b></small></center>
         </div>
     `;
+
+    // Tambahkan event listener untuk Total Top-Up
+    const totalTopUpElement = document.querySelector('.total-topup');
+    totalTopUpElement.addEventListener('click', () => {
+        totalTopUpElement.classList.toggle('censored');
+    });
 }
 
 $(document).ready(function () {
